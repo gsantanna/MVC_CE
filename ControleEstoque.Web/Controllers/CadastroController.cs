ccbddafd407bca6ee8e6b8cd3a9508e690765e88
Gustavo da Cunha Santanna <provider.gsantanna@terceiros.mongeralaegon.com.br>
2019-04-28 19:57:51 -0300

    Funcionalidade dos botões Salvar e Alterar para o Cadastro de Grupos de Produtos

--- ControleEstoque.Web/Controllers/CadastroController.cs
+++ ControleEstoque.Web/Controllers/CadastroController.cs
@@ -29,6 +29,43 @@ namespace ControleEstoque.Web.Controllers
             return Json(_listaGrupoProduto.Find(x => x.Id == id));
         }
 
+        [HttpPost]
+        [Authorize]
+        public ActionResult ExcluirGrupoProduto(int id)
+        {
+            var ret = false;
+
+            var registroBd = _listaGrupoProduto.Find(x => x.Id == id);
+            if (registroBd != null)
+            {
+                _listaGrupoProduto.Remove(registroBd);
+                ret = true;
+            }
+
+            return Json(ret);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult SalvarGrupoProduto(GrupoProdutoModel model)
+        {
+            var registroBd = _listaGrupoProduto.Find(x => x.Id == model.Id);
+
+            if (registroBd == null)
+            {
+                registroBd = model;
+                registroBd.Id = _listaGrupoProduto.Max(x => x.Id) + 1;
+                _listaGrupoProduto.Add(registroBd);
+            }
+            else
+            {
+                registroBd.Nome = model.Nome;
+                registroBd.Ativo = model.Ativo;
+            }
+
+            return Json(registroBd);
+        }
+
         [Authorize]
         public ActionResult MarcaProduto()
         {
